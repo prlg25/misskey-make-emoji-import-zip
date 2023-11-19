fa7e83218ce67295da53d7b4ad02815b0df699cf
prlg25 <ynkthv@gmail.com>
2023-11-19 16:23:52 +0900

    yaml形式のファイルを読み込み、タグも生成するように変更

--- README.md
+++ README.md
@@ -5,25 +5,49 @@ Misskeyのカスタム絵文字をバルクインポートするためのZIPフ�
 - Python3.11(古いバージョンでも動くかもですが動作確認してないです)
 - Windows11(他のOSでも動くと思いますが動作確認してないです)
 
+## 機能
+- 所定の形式のyamlと画像を用意すれば、絵文字import用zipを生成できます
+- [pykakasi](https://pypi.org/project/pykakasi/)を使って読み仮名のタグを自動生成します
+    - pykakasiの癖を御しきれていないので"どぅ"などの特殊表記を中心に変な自動生成になります
+
 ## 使い方
-1. 以下のように画像ファイルを配置する
+1. 以下のようなyaml形式のフォーマットで絵文字申請してもらう
+    - 半角スペースを抜かさないようにしてもらいましょう
+    ```
+    ====ここからテンプレ====
+    emojiname:
+      テキスト: テキスト
+      読み仮名: てきすと
+      その他つけたいタグ: タグ1,タグ2
+      カテゴリ名: Japanese_words
+      使用フォント: フォント名(ある場合)
+    ====ここまでテンプレ====
+    ```
+2. 以下のように画像ファイルを配置する
+    - 画像ファイル名とyamlのemojinameを対応させるようにしましょう
     ```
     .emojidir
         emoji1.png
         emoji2.png
         emoji3.gif
     ```
-2. CLIを開いてスクリプトを実行する
+3. CLIを開いてスクリプトを実行する
     ```
     # Windowsの場合
-    python C:\path\to\scriptfile\make_import_zipfile.py --datadir C:\path\to\emojidir
+    python C:\path\to\scriptfile\make_import_zipfile.py --datadir C:\path\to\emojidir --yamlpath C:\path\to\yamlpath.yaml
     ```
-3. Zipファイルが吐かれるのでこれをMisskeyにインポートする
-    1. コントロールパネルのカスタム絵文字ページの「・・・」をクリック
-    2. インポート⇒アップロード
+    - C:\path\to\emojidir\meta.jsonが生成されるので問題ないか確認します
+
+4. 生成されたzipファイルをMisskeyにインポートする
+    1. C:\path\to\emojidir\import_zipfile.zipにインポート用ZIPファイルが生成されます
+    2. コントロールパネルのカスタム絵文字ページの「・・・」をクリック
+    3. インポート⇒アップロード
 
 ## 備考
 - 対応する拡張子を増やすときはpyファイルをいじってください
+
+## ライセンス
+- pykakasiがGPLライセンスのためGPLライセンスとしています
 
 ## 参考
 - https://scrapbox.io/defaultcf/Misskey%E3%81%A7%E3%82%AB%E3%82%B9%E3%82%BF%E3%83%A0%E7%B5%B5%E6%96%87%E5%AD%97%E3%82%92%E4%B8%80%E6%B0%97%E3%81%AB%E5%85%A5%E3%82%8C%E3%82%8B
--- make_import_zipfile.py
+++ make_import_zipfile.py
@@ -4,11 +4,96 @@ import zipfile
 import argparse
 from pathlib import Path
 
+import yaml
+import pykakasi
+import re
+
+kunrei2hepburn = [
+    {
+        r'sya': 'sha',
+        r'si': 'shi',
+        r'syu': 'shu',
+        r'sye': 'she',
+        r'syo': 'sho',
+    },
+    {
+        r'zzya': 'jja',
+        r'zzi': 'jji',
+        r'zzyu': 'jju',
+        r'zzye': 'jje',
+        r'zzyo': 'jjo',
+        r'zya': 'ja',
+        r'zi': 'ji',
+        r'zyu': 'ju',
+        r'zye': 'je',
+        r'zyo': 'jo',
+    },
+    {
+        r'ttya': 'ccha',
+        r'tti': 'cchi',
+        r'ttyu': 'cchu',
+        r'ttye': 'cche',
+        r'ttyo': 'ccho',
+        r'tya': 'cha',
+        r'ti': 'chi',
+        r'tyu': 'chu',
+        r'tye': 'che',
+        r'tyo': 'cho',
+    },
+    {
+        r'tu': 'tsu',
+    },
+    {
+        r'ffu': 'hhu',
+        r'fu': 'hu',
+    }
+]
+
+def read_emoji_info_text(text_file_path):
+    with open(text_file_path, "r", encoding="utf-8") as f:
+        loaded_yaml = yaml.safe_load(f)
+        # print(loaded_yaml)
+    return loaded_yaml
+
+def replace_from_dict(input_string:str, replacement_patterns:dict):
+    for pattern, replacement in replacement_patterns.items():
+        input_string = re.sub(pattern, replacement, input_string)
+        # print(input_string)
+    return input_string
+
+def generate_tag_text_from_hiragana(hiragana_text):
+    kks = pykakasi.kakasi()
+    tag_set = set()
+    hiragana_text = hiragana_text.replace("ー", "-")
+    hiragana_text = re.sub(r"(っ|ッ)(ぢ|ヂ)", "ddi", hiragana_text)
+    hiragana_text = re.sub(r"(ぢ|ヂ)", "di", hiragana_text)
+    hiragana_text = re.sub(r"(っ|ッ)(づ|ヅ)", "ddu", hiragana_text)
+    hiragana_text = re.sub(r"(づ|ヅ)", "du", hiragana_text)
+    # hiragana_text = hiragana_text.replace("ー", "-")
+    result = kks.convert(hiragana_text)
+    print(result)
+    kunrei_text = ""
+    for each in result:
+        kunrei_text += each["kunrei"]
+    tag_set.add(kunrei_text)
+    print(tag_set)
+    for each_pattern in kunrei2hepburn:
+        append_text_list = []
+        for each_text in tag_set:
+            append_text_list.append(replace_from_dict(each_text, each_pattern))
+        # print(each_pattern)
+        # print(append_text_list)
+        for each in append_text_list:
+            tag_set.add(each)
+    print(tag_set)
+    return list(tag_set)
+
 
 def main():
     # コマンドライン引数の処理
     parser = argparse.ArgumentParser()
     parser.add_argument('--datadir', type=Path, required=True)
+    parser.add_argument('--yamlpath', type=Path, required=False)
     args = parser.parse_args()
     
     # JSON作成
@@ -31,12 +116,55 @@ def main():
             }
             emojis_list.append(each_json)
 
+    if args.yamlpath:
+        # ファイル読み込み
+        print("絵文字情報をロードします")
+        loaded_emojiinfo = read_emoji_info_text(args.yamlpath)
+        # print(loaded_emojiinfo)
+        # each_jsonについて、fileNameがyamlに含まれる場合、
+        for i, each_json in enumerate(emojis_list):
+            print(f'{each_json["emoji"]["name"]}を処理中')
+            if each_json["emoji"]["name"] in loaded_emojiinfo.keys():
+                each_emojiinfo = loaded_emojiinfo[each_json["emoji"]["name"]]
+                tag_text = []
+                if each_emojiinfo["読み仮名"]:
+                    yomigana_list = each_emojiinfo["読み仮名"].split(",")            
+                    for yomigana in yomigana_list:
+                        # 読み仮名のテキストからタグを生成
+                        tag_text.extend(generate_tag_text_from_hiragana(yomigana))
+                        # 読み仮名をタグに追加
+                        tag_text.append(yomigana)
+                if each_emojiinfo["テキスト"]:
+                    # テキストをタグに追加
+                    text_tags = str(each_emojiinfo["テキスト"]).split(",")
+                    removed_blank_text_tags = []
+                    for each in text_tags:
+                        removed_blank_each = re.sub(r"( |　)", "", each)
+                        removed_blank_text_tags.append(removed_blank_each)
+                    # tag_text.extend(str(each_emojiinfo["テキスト"]).split(","))
+                    tag_text.extend(removed_blank_text_tags)
+                # その他つけたいタグがあればタグに追加
+                if each_emojiinfo["その他つけたいタグ"]:
+                    other_tags = str(each_emojiinfo["その他つけたいタグ"]).split(",")
+                    removed_blank_other_tags = []
+                    for each in other_tags:
+                        removed_blank_other_each = re.sub(r"( |　)", "", each)
+                        removed_blank_other_tags.append(removed_blank_other_each)
+                    # tag_text.extend(str(each_emojiinfo["その他つけたいタグ"]).split(","))
+                    tag_text.extend(removed_blank_other_tags)
+                # 重複削除
+                tag_text = sorted(set(tag_text), key=tag_text.index)
+                emojis_list[i]["emoji"]["aliases"] = tag_text
+                # カテゴリがあればカテゴリを追加
+                if each_emojiinfo["カテゴリ名"]:
+                    emojis_list[i]["emoji"]["category"] = each_emojiinfo["カテゴリ名"]
+    
     output_base_json = {
         "emojis": emojis_list
     }
     # meta.json出力
-    with open(datadir.joinpath("meta.json"), "w") as f:
-        json.dump(output_base_json, f)
+    with open(datadir.joinpath("meta.json"), "w", encoding="utf-8") as f:
+        json.dump(output_base_json, f, ensure_ascii=False)
 
     # zip出力
     zipfile_path = datadir.joinpath("import_zipfile.zip")
